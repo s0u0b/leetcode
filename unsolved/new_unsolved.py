b2ede43c4764859144d6c1e5df6996d73bf2154c
s0u0b <s0u0b0630@gmail.com>
2022-05-09 07:02:08 +0800

    refactor write to file

--- unsolved/new_unsolved.py
+++ unsolved/new_unsolved.py
@@ -32,29 +32,38 @@ def main():
                 print(f'Create {file_path} successfully')
 
             with open(file_path, 'w') as file:
-                print('"""', file=file)
                 question_frontend_id = crawler.get_frontend_id(problem)
                 question_title = crawler.get_title(problem)
-                print(f'Problem:\n    {question_frontend_id}. {question_title}', file=file)
                 difficulty = crawler.get_difficulty(problem)
-                print(f'Difficulty:\n    {difficulty}', file=file)
                 url = crawler.get_url(problem)
-                print(f'URL:\n    {url}', file=file)
                 topic_tags = crawler.get_topic_tags(problem)
-                print(f'Tags:\n    {topic_tags}', file=file)
-                print('"""', file=file)
                 code = crawler.get_python_code(problem)
-                print(code, file=file)
                 test_cases = crawler.get_test_cases(problem)
-                print(test_cases, file=file)
+                print('"""', file=file)
+                print(f'Problem:\n'
+                      f'    {question_frontend_id}. {question_title}\n'
+                      f'Difficulty:\n'
+                      f'    {difficulty}\n'
+                      f'URL:\n'
+                      f'    {url}\n'
+                      f'Tags:\n'
+                      f'    {topic_tags}\n'
+                      f'"""\n'
+                      f'\n'
+                      f'\n'
+                      f'{code}\n'
+                      f'\n'
+                      f'\n'
+                      f'{test_cases}\n'
+                      f'\n', file=file)
                 if args.validator:
-                    print('\n\n', file=file)
                     function_name = crawler.get_function_name(problem)
                     parameter_names = crawler.get_parameter_names(problem)
-                    print(f'def validator({function_name}, inputs, expected):', file=file)
-                    print(f'    {parameter_names} = inputs', file=file)
-                    print(f'    output = {function_name}({parameter_names})', file=file)
-                    print(f'    assert output == expected, (output, expected)', file=file)
+                    print(f'def validator({function_name}, inputs, expected):\n'
+                          f'    {parameter_names} = inputs\n'
+                          f'    output = {function_name}({parameter_names})\n'
+                          f'    assert output == expected, (output, expected)\n'
+                          f'\n', file=file)
 
 
 if __name__ == '__main__':
